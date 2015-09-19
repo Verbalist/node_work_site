7f2c6532d6483201a90ebeb798bae70298c43c3e
verbalist <enshteynn@ukr.net>
2015-09-19 12:38:32 +0300

    a

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -17,8 +18,10 @@ var auth = require('./routes/auth')
 var app = express();
 
 // view engine setup
+app.engine('html', swig.renderFile);
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
