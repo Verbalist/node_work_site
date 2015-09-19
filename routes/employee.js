696ecbd58115c620bb99126295679b95497e0647
joby <jobyand121@gmail.com>
2015-09-19 09:38:53 +0300

    fixes

--- routes/employee.js
+++ routes/employee.js
@@ -5,32 +5,32 @@ var router = express.Router();
 router.get('/:name', function(req, res, next) {
 	console.log('/:name')
 	console.log(req.params.name)
-  res.render('index', { title: 'Express' });
+  res.render('employee/employee_index');
 });
 
 router.get('/:name/resumes', function(req, res, next) {
 	console.log('/:name/resumes')
 	console.log(req.params.name + " resumes")
 	console.log(req.query.id)
-  res.render('index', { title: 'Express' });
+  res.render('employee/employee_resumes');
 });
 
-router.post('/:name/resume/new', function(req, res, next) {
+router.get('/:name/resume/new', function(req, res, next) {
 	console.log('/:name/resume/new')
-	console.log(req.params.name + " resumes/new")
-  	res.render('index', { title: 'Express' });
+	console.log(req.params.name + " resume/new")
+  	res.render('employee/employee_resume_new');
 });
 
-router.post('/:name/resume/edit', function(req, res, next) {
+router.get('/:name/resume/edit', function(req, res, next) {
 	console.log('/:name/resume/edit')
 	console.log(req.params.name)
-  	res.render('index', { title: 'Express' });
+  	res.render('employee/employee_resume_edit');
 });
 
 router.get('/:name/profile', function(req, res, next) {
 	console.log('/:name/profile/')
 	console.log(req.params.name)
-  res.render('index', { title: 'Express' });
+  res.render('employee/employee_profile');
 });
 
 module.exports = router;
