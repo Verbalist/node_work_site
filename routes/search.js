a8b8631107f4dc099ff04ab9825a9ee61cb31cf2
verbalist <enshteynn@ukr.net>
2015-09-19 14:05:21 +0300

    a

--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET search employee */
 router.get('/:category/:name', function(req, res, next) {
 	console.log('search/' + req.params.category + '/' + req.params.name)
-  res.render('search', {title: 'search', 
+  res.render('search/search', {title: 'search', 
   						category: 'category: ' + req.params.category,
   						name: 'name: ' + req.params.name,
   						});
