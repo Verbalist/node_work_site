c087db512ffa63c7332ebfdd0aa52027b4347fd5
joby <jobyand121@gmail.com>
2015-09-20 10:58:30 +0300

    bolshoi molodez

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var swig = require('swig')
+//var engine = require('ejs-locals');
 
 var index = require('./routes/index');
 var employee = require('./routes/employee')
@@ -15,10 +15,15 @@ var auth = require('./routes/auth')
 var app = express();
 
 // view engine setup
-app.engine('html', swig.renderFile);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+// set the view engine to ejs
+//app.engine('html', require('ejs').renderFile);
+//app.engine('ejs', engine);
 
+
+//app.set('view engine', 'html');
+//app.engine('html', swig.renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
