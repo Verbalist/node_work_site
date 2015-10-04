dad68ea34fc7e3acafafde53ee69e547fde53eeb
verbalist <enshteynn@ukr.net>
2015-10-04 22:38:32 +0300

    a

--- routes/api_auth.js
+++ routes/api_auth.js
@@ -1,23 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var crypto = require('crypto')
 
 router.post('/auth/login', function(req, res, next) {
 	console.log('back /auth/login')
 	//some logic
 	var result = {};
+	// console.log(req)
 	try {
-		if (req.query.login.length > 3 && req.query.password == 123) {
+		if (req.body.login.length > 3 && req.body.password == 123) {
 			result.success = true;
-			hash = crypto.randomBytes(32);
-	  		token = hash.toString('hex');
-			res.cookie('node_sessid', token);
-			res.cookie('auth', true)
-			if (req.query.login.length == 5) {
-				res.cookie('role', 'employer')
+			if (req.body.login.length == 5) {
+				result.role = 'employer';
 			} else {
-				res.cookie('role', 'employee')
+				result.role = 'employee';
 			}
 		} else {
 			result.success = false;
@@ -34,34 +30,13 @@ router.post('/auth/login', function(req, res, next) {
   res.json(result);
 });
 
-router.post('/auth/logout', function(req, res, next) {
-	console.log('back /auth/logout')
-	//some logic
-	var result = {};
-	try {
-		result.error_code = 0;
-		result.success = true;
-		console.log('cookie before: ' + req.cookies.role)
-		res.clearCookie('node_sessid');
-		res.cookie('auth', false);
-		res.clearCookie('role', 'employee');
-	}
-	catch (e) {
-		console.log(e);
-		result.error_code = 1;
-		result.success = false;
-	}
-	console.log(result);
-  res.json(result);
-});
-
 router.post('/auth/registration', function(req, res, next) {
 	console.log('back /auth/registration')
 	var result = {};
 
 	//some logic
-	var a = req.query;
-	console.log(req.query);
+	var a = req.body;
+	console.log(req.body);
 	result.error_code = 0;
 	result.success = true;
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,74 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
+var crypto = require('crypto');
 
 /* GET login */
 router.get('/login', function(req, res, next) {
-	console.log('front auth/login')
+	console.log('front GET auth/login');
+	console.log(req.cookies.auth);
   res.render('auth/login', {auth: req.cookies.auth || false, role: req.cookies.role || 'employee'});
+});
+
+/* POST login */
+router.post('/login', function(req, res, next) {
+	console.log('front POST auth/login');
+
+	try {
+		// console.log(req.body.login);
+		rp({
+			method: 'POST', 
+			uri: 'http://localhost/api/auth/login',
+			json: true,
+			body: {
+			// formData: {
+				login: req.body.login,
+				password: req.body.password
+			}
+		}).then(function(data) { 
+			if (data.success) {
+				hash = crypto.randomBytes(32);
+		  		token = hash.toString('hex');
+				res.cookie('node_sessid', token);	
+				res.cookie('auth', true);
+				res.cookie('role', data.role);
+			 	res.redirect('login');
+			} else {
+				res.json(data);
+			}
+		})
+		.catch(function(error) {
+			console.log(error);
+			res.json({success: false, error_code: 2});
+		});
+	}
+	catch (e){
+		console.log(e);
+	}
+  	// res.render('auth/login', {auth: req.cookies.auth || false, role: req.cookies.role || 'employee'});
+});
+
+// GET logout
+router.get('/logout', function(req, res, next) {
+	console.log('front GET /auth/logout')
+	//some logic
+	var result = {};
+	try {
+		result.error_code = 0;
+		result.success = true;
+		console.log('cookie before: ' + req.cookies.role)
+		res.clearCookie('node_sessid');
+		res.cookie('auth', false);
+		res.cookie('role', 'employee');
+		res.redirect('login');
+	}
+	catch (e) {
+		console.log(e);
+		result.error_code = 1;
+		result.success = false;
+	}
+	console.log(result);
+  res.json(result);
 });
 
 /* GET registration */
