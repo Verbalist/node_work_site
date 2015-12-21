9b6dfb90e9bfadfadd413317f6690d39cdae7c22
joby <jobyand121@gmail.com>
2015-12-21 13:29:14 +0200

    fix

--- routes/api_employer.js
+++ routes/api_employer.js
@@ -2,129 +2,152 @@ var express = require('express');
 var router = express.Router();
 var requestMaker = require('../libs/requestMaker.js');
 
-router.post('/employer/positions', function(req, res, next) {
-	console.log('/employer/positions')
-	var result = {};
-	var request = {};
-	//some logic
-	request.id = req.session.customer_id; // enter id from session here
-	console.log("wtf - " + request.id);
-	requestMaker.post('/employer/getPositions', request, 
-		function (result) {
-			console.log(result)
-		  	res.json(result);
-  	});
-});
+var requireRole = function(role) {
+  return function(req, res, next) {
+    if('user' in req.session && req.session.user.role === role)
+      next();
+    else{
+	  	console.log('ERRRRROR')
+	  	res.sendStatus(403);	
+    }
+  }
+};
 
-router.get('/employer/position/:id', function(req, res, next) {
-	console.log('/employer/position/' + req.params.id)
-	var ans = {};
+var employer = {
+	positions : [requireRole('employer'), function(req, res, next) {
+		console.log('/employer/positions')
+		var result = {};
+		var request = {};
+		//some logic
+		request.id = req.session.user.id; // enter id from session here
+		console.log("wtf - " + request.id);
+		requestMaker.post('/employer/getPositions', request, 
+			function (result) {
+				console.log(result)
+			  	res.json(result);
+	  	});
+	}],
+	position : [requireRole('employer'), function(req, res, next) {
+		console.log('/employer/position/' + req.params.id)
+		var ans = {};
 
-	//some logic
-	ans.error_code = 0;
-	var request = {};
-	request.id = req.session.customer_id; // enter id from session here
-	requestMaker.post('/position/getPosition', request, 
-		function (result) {
-		  	result.total = 1;
-		  	res.json(result);
-  	});
-});
+		//some logic
+		ans.error_code = 0;
+		var request = {};
+		request.id = req.session.user.id; // enter id from session here
+		requestMaker.post('/position/getPosition', request, 
+			function (result) {
+			  	result.total = 1;
+			  	res.json(result);
+	  	});
+	}],
+	deletePositions : [requireRole('employer'), function(req, res, next) {
+		console.log('/employer/deletePositions/' + req.params.id)
+			var result = {};
+			
+			//some logic
+			
+			result.error_code = 0;
+		  res.json(result);
+	}],
+	updatePositions : [requireRole('employer'), function(req, res, next) {
+		console.log('/employer/updatePositions/' + req.params.id)
+		var result = {};
+		
+		console.log(req.body)
 
-router.post('/employer/deletePositions/:id', function(req, res, next) {
-	console.log('/employer/deletePositions/' + req.params.id)
-	var result = {};
-	
-	//some logic
-	
-	result.error_code = 0;
-  res.json(result);
-});
+		//some logic
+		var request = JSON.parse(req.body.json);
+		console.log(request);
+		request.id = req.params.id;
+		request.creator_id = req.session.user.id; // enter id from session here
+		console.log(request);
 
-router.post('/employer/updatePositions/:id', function(req, res, next) {
-	console.log('/employer/updatePositions/' + req.params.id)
-	var result = {};
-	
-	console.log(req.body)
+		requestMaker.post('/position/updatePosition', request, 
+			function (result) {
+				console.log(result);
+			  	res.json(result);
+	  	});
+	  	
+		result.error_code = 0;
+	  res.json(result);
+	}],
+	newPosition : [requireRole('employer'), function(req, res, next) {
+		console.log('/position/newPosition')
+		var result = {};
+		try{
+			console.log(JSON.parse(req.body.json))
+			//some logic
+			var request = JSON.parse(req.body.json);
 
-	//some logic
-	var request = JSON.parse(req.body.json);
-	console.log(request);
-	request.id = req.params.id;
-	request.creator_id = req.session.customer_id; // enter id from session here
-	console.log(request);
+			request.creator_id = req.session.user.id; // enter id from session here
+			requestMaker.post('/position/newPosition', request, 
+				function (result) {
+					res.json(result);
+			});
+		} catch (err){
+			console.log(err)
+		}
+	}],
+	profile : [requireRole('employer'), function(req, res, next) {
+		console.log('/employer/profile')
+		var result = {};
 
-	requestMaker.post('/position/updatePosition', request, 
-		function (result) {
-			console.log(result);
-		  	res.json(result);
-  	});
-  	
-	result.error_code = 0;
-  res.json(result);
-});
+		//some logic
+		var profile_info = {
+				"id":"1",
+				"name":"BigCompany",
+				"category":"it",
+				"status":"looking for slaves",
+				"email":"example@gmail.com",
+				"phone":"1234567"
+			};
+		result.error_code = 0;
+		result.profile_info = profile_info;
 
-router.post('/employer/newPosition', function(req, res, next) {
-	console.log('/position/newPosition')
-	var result = {};
-	try{
-	console.log(JSON.parse(req.body.json))
-	//some logic
-	var request = JSON.parse(req.body.json);
+		console.log(result);
+	  res.json(result);
+	}],
+	updateProfile : [requireRole('employer'), function(req, res, next) {
+		console.log('/employer/updateProfile')
+		var result = {};
 
-	request.creator_id = req.session.customer_id; // enter id from session here
-	requestMaker.post('/position/newPosition', request, 
-		function (result) {
-			res.json(result);
-	});
-} catch (err){
-	console.log(err)
+		console.log(req.body)
+		
+		//some logic
+
+		result.error_code = 0;
+
+		console.log(result);
+	  res.json(result);
+	}],
+	deleteCustomer : [requireRole('employer'), function(req, res, next) {
+		console.log('/employer/deleteCustomer' + req.params.id)
+		var result = {};
+		
+		//some logic
+		
+		result.error_code = 0;
+	  res.json(result);
+	}],
 }
-});
 
 
-router.get('/employer/profile', function(req, res, next) {
-	console.log('/employer/profile')
-	var result = {};
+router.post('/employer/positions', employer.positions);
 
-	//some logic
-	var profile_info = {
-			"id":"1",
-			"name":"BigCompany",
-			"category":"it",
-			"status":"looking for slaves",
-			"email":"example@gmail.com",
-			"phone":"1234567"
-		};
-	result.error_code = 0;
-	result.profile_info = profile_info;
+router.get('/employer/position/:id', employer.position);
 
-	console.log(result);
-  res.json(result);
-});
+router.post('/employer/deletePositions/:id', employer.deletePositions);
 
-router.post('/employer/updateProfile', function(req, res, next) {
-	console.log('/employer/updateProfile')
-	var result = {};
+router.post('/employer/updatePositions/:id', employer.updatePositions);
 
-	console.log(req.body)
-	
-	//some logic
+router.post('/employer/newPosition', employer.newPosition);
 
-	result.error_code = 0;
 
-	console.log(result);
-  res.json(result);
-});
+router.get('/employer/profile', employer.profile);
 
-router.post('/employer/deleteCustomer', function(req, res, next) {
-	console.log('/employer/deleteCustomer' + req.params.id)
-	var result = {};
-	
-	//some logic
-	
-	result.error_code = 0;
-  res.json(result);
-});
+router.post('/employer/updateProfile', employer.updateProfile);
+
+router.post('/employer/deleteCustomer', employer.deleteCustomer);
 
 module.exports = router;
--- routes/employer.js
+++ routes/employer.js
@@ -1,36 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+var requireRole = function(role) {
+  return function(req, res, next) {
+    if('user' in req.session && req.session.user.role === role)
+      next();
+    else{
+	  	console.log('ERRRRROR')
+	  	res.redirect('/search');	
+    }
+  }
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', [requireRole('employer'), function(req, res, next) {
 	console.log('/')
 	console.log(req.params.name)
-  res.render('employer/employer_index', {auth: req.cookies.auth || false, role: req.cookies.role || 'employer'});
-});
+  res.render('employer/employer_index', {auth: true, role: req.session.user.role});
+}]);
 
-router.get('/positions', function(req, res, next) {
+router.get('/positions', [requireRole('employer'), function(req, res, next) {
 	console.log('/positions')
 	console.log(req.params.name)
 	console.log(req.query.ids)
-  res.render('employer/employer_positions', {auth: req.cookies.auth || false, role: req.cookies.role || 'employer'});
-});
+  res.render('employer/employer_positions', {auth: true, role: req.session.user.role});
+}]);
 
-router.get('/position/new', function(req, res, next) {
+router.get('/position/new', [requireRole('employer'), function(req, res, next) {
 	console.log('/position/new')
 	console.log(req.params.name)
-  res.render('employer/employer_position_new', {auth: req.cookies.auth || false, role: req.cookies.role || 'employer'});
-});
+  res.render('employer/employer_position_new', {auth: true, role: req.session.user.role});
+}]);
 
-router.get('/position/:position_id', function(req, res, next) {
+router.get('/position/:position_id', [requireRole('employer'), function(req, res, next) {
 	console.log('/:name/position/edit/:position_id')
 	console.log(req.params.name)
-  res.render('employer/employer_position_edit', {auth: req.cookies.auth || false, role: req.cookies.role || 'employer'});
-});
+  res.render('employer/employer_position_edit', {auth: true, role: req.session.user.role});
+}]);
 
-router.get('/profile', function(req, res, next) {
+router.get('/profile', [requireRole('employer'), function(req, res, next) {
 	console.log('/profile/')
 	console.log(req.params.name)
-  res.render('employer/employer_profile', {auth: req.cookies.auth || false, role: req.cookies.role || 'employer'});
-});
+  res.render('employer/employer_profile', {auth: true, role: req.session.user.role});
+}]);
 
 module.exports = router;
