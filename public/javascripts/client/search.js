f4aafb1713377223b75663d4df67c8b57abec083
Droom1995 <husiev95@ukr.net>
2015-12-27 21:52:12 +0200

    find_added

--- public/javascripts/client/search.js
+++ public/javascripts/client/search.js
@@ -3,11 +3,9 @@ function showSearchPositions(){
     var url = window.location.href;
     var split_url = url.split("/");
     console.log(split_url[split_url.length-2]);
-    page_number = 1;//$('#page_number')[0].value;
-    positions_per_page = 100;//$('#positions_per_page')[0].value;
-    start = (page_number-1)*positions_per_page;
-    limit = positions_per_page;
-    var response = getPositionsSearch(limit, start);
+    //$('#page_number')[0].value;
+    text_string = $('#search_text')[0].value;
+    var response = getPositionsSearch(text_string);
     var template = $("#showPositionsEmployer").html();
     for(var elem in response.positions){
         var html = Mustache.render(template, response.positions[elem]);
@@ -42,4 +40,44 @@ function getResumesSearch(imit,start){
     }
 
     return result;
+}
+
+function getPositionsSearch(text_string){
+    console.log("getPositionsSearch - Started!!!");
+
+    var requestArray = {};
+    requestArray.text_string = text_string;
+
+    var URL = "/search";
+
+     var requestBody = 'json=' + encodeURIComponent(JSON.stringify(requestArray));
+    //  Send request
+    var result = doRequestAjaxPostSearch(requestBody, URL, "POST", false).responseJSON;
+    checkUndefined(result);
+    console.log(result);
+    return result;
+}
+
+function checkUndefined(result,error_obj){
+    var serverErrorLabel = $('<label/>', {
+                    id: 'server_connection_error',
+                    "class": 'user-server-error'
+                });
+    if(result !== undefined) {
+        if (result.error_code != 0) {
+            console.log("Error code = " + result.error_code);
+        }
+        if(error_obj !== undefined){
+            switch (result.error_code) {
+                        case 403:
+                            serverErrorLabel.text(error_obj.error_text);
+                            if(error_obj.error_template !== undefined){
+                                serverErrorLabel.appendTo(error_obj.error_template);
+                            }
+                            break;
+                }
+        }
+    } else {
+        console.log("Server connection error");
+    }
 }
--- routes/api_search.js
+++ routes/api_search.js
@@ -1,4 +1,37 @@
 var express = require('express');
-var request = require('request');
 var router = express.Router();
 var requestMaker = require('../libs/requestMaker.js');
+
+var requireRole = function(role) {
+  return function(req, res, next) {
+    if(role === 'all'){
+    	next();
+    }else if('user' in req.session && req.session.user.role === role)
+      next();
+    else{
+	  	console.log('ERRRRROR')
+	  	res.sendStatus(403);	
+    }
+  }
+};
+
+var search = {
+	positions : [requireRole('all'), function(req, res, next) {
+		console.log('/search')
+		var result = {};
+		var request = {};
+		//some logic
+		request.id = req.session.user.id; // enter id from session here
+		console.log("wtf - " + request.id);
+		requestMaker.post('/position/getAll', request, 
+			function (result) {
+				console.log(result)
+			  	res.json(result);
+	  	});
+	}]
+}
+
+
+router.post('/search', search.positions);
+
+module.exports = router;
